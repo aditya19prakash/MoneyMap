c6884d85c0b8851b67dd0692616373ca49d68a2b
aditya19prakash <adyprakash19@gmail.com>
2025-03-24 14:33:00 +0530

    Add Flask application setup and initial route; create config and template files

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask import Flask
 it creates an instance of the flask class,
 which will be your WsGI(web server Gateqway Interface) application
 '''
+###WSGI application
+app = Flask(__name__)
 
-app=Flask()
+@app.route("/")
+def welcome():
+    return "welcome to this flask course 1d234"
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
